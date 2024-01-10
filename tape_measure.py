f35100e68c36bcf8ef26078b483a981ad286ff3a
f9ki3 <floterina@gmail.com>
2024-01-10 10:57:21 +0800

    update tape measure app

--- tape_measure.py
+++ tape_measure.py
@@ -1,52 +1,98 @@
-# by the default 1 feet is 12 inches
-# and 4 inch is 10 cm
-
 def main():
-    greet()
     while True:
+        greet()
         user_input = input("action:")
         if user_input == 's':
             menu()
             action = input("select unit: ")
-            convert(action)
+            if action == 'i' or action == 'f' or action == 'c':
+                convert(action)
+            else:
+                print("-----------------------------------\nInvalid Input")
         elif user_input == 'e':
-            print("exited!")
+            print("-----------------------------------\nyou exited!")
             return False
         else:
-            print("Invalid Input")
+            print("-----------------------------------\nInvalid Input")
             
 
 def greet():
     print('''
-    -----------------------------------
-    | Welcome to Tape Measure App     |
-    | press (s) to start              |
-    | press (e) to exit               |    
-    -----------------------------------
+-----------------------------------
+| Welcome to Tape Measure App     |
+| by f9ki3\n                      |
+| press (s) to start              |
+| press (e) to exit               |    
+-----------------------------------
     ''')
 def menu():
     print('''
-    -----------------------------------
-    | Select action here:             | 
-    | press (i) to convert inch       |  
-    | press (c) to convert centimeter |  
-    | press (f) to convert foot       | 
-    -----------------------------------
+-----------------------------------
+| Select action here:             | 
+| press (i) to convert inch       |  
+| press (c) to convert centimeter |  
+| press (f) to convert foot       | 
+-----------------------------------
           ''')
 
 def convert(action):
-    #inch to ft = 1 inch is 0.0833333333
-    
+    if action == "i":
+        while True:
+            unitToConvert = unit()
+            if unitToConvert[0] == "c":
+                print("Result: ", str(round(unitToConvert[2], 2)) + "cm")
+                break
+            elif unitToConvert[0] == "f":
+                print("Result: ", str(round(unitToConvert[3], 2))+ "ft")
+                break
+            elif unitToConvert[0] == "i":
+                print("Result: ",str(round(unitToConvert[1]))+ "inch")
+                break
+            else: 
+                print("Invalid Input")
+                break
+    elif action == "c":
+        while True:
+            unitToConvert = unit()
+            if unitToConvert[0] == "i":
+                print("Result: ", str(round(unitToConvert[5], 2)) + "inch")
+                break
+            elif unitToConvert[0] == "f":
+                print("Result: ", str(round(unitToConvert[4], 2)) + "ft")
+                break
+            elif unitToConvert[0] == "c":
+                print("Result: ", str(round(unitToConvert[1])) + "cm")
+                break
+            else: 
+                print("Invalid Input")
+                break
+    elif action == "f":
+        while True:
+            unitToConvert = unit()
+            if unitToConvert[0] == "i":
+                print("Result: ", str(round(unitToConvert[6], 2)) + "inch")
+                break
+            elif unitToConvert[0] == "c":
+                print("Result: ", str(round(unitToConvert[7], 2)) + "cm")
+                break
+            elif unitToConvert[0] == "f":
+                print("Result: ", str(round(unitToConvert[1])) + "ft")
+                break
+            else: 
+                print("Invalid Input")
+                break
+            
+def unit():
     unit = input("select unit to convert: ")
     value = float(input("Enter the value: "))
     
-    if action == "i":
-        inchToCentimeter = value * 2.54
-        inchTofoot = value * 0.0833333333
+    inchToCentimeter = value * 2.54
+    inchTofoot = value * 0.0833333333
+    centimeterToFoot = value * 0.0328
+    centimeterToInch = value * 0.393700787
+    feetToInch = value * 12
+    feetToCentimeter = value * 30.48   
     
-        print(f'''
-            {round(inchToCentimeter)}, {round(inchTofoot)}
-        ''')
-    
+    return unit, value, inchToCentimeter, inchTofoot, centimeterToFoot, centimeterToInch, feetToInch, feetToCentimeter
 
 main()
