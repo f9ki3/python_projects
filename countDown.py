0fcd5517ea8c38ccfc188214f0a7312280161db3
Fyke Lleva <floterina@gmail.com>
2024-06-25 01:26:53 +0800

    countDownProgram

--- countDown.py
+++ countDown.py
@@ -1,14 +1,18 @@
-import time
-
+import time,os, platform
+from pyfiglet import Figlet
 def main():
     timeInput = int(input("Enter number of seconds: "))
     countDown(timeInput)
 
 def countDown(timeInput):
-    for second in range(0,timeInput,1):
+    for second in range(timeInput,0,-1):
+        os.system('clear')
+        textStyle = Figlet(font='slant').renderText(str(second))
+        print(textStyle)
         time.sleep(1)
-        print(timeInput)
         timeInput -= 1
+    os.system('clear')
+    print(Figlet(font='slant').renderText(str('Times Up!')))
 
 if __name__ == "__main__":
     main()
